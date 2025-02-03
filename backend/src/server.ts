e84cd0ab2ee9ac7f83ee6329912ccdb54a747f7b
A-Smith-01 <alex3.141smith@gmail.com>
2025-02-03 19:31:51 +0000

    fixed scheduled date not having a default value when updated

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -53,6 +53,9 @@ server.put<{ Params: { id: string }; Body: Entry }>("/update/:id", async (req, r
   updatedEntryBody.created_at
     ? (updatedEntryBody.created_at = new Date(req.body.created_at))
     : (updatedEntryBody.created_at = new Date());
+  updatedEntryBody.scheduled
+  ? (updatedEntryBody.scheduled = new Date(req.body.scheduled))
+  : (updatedEntryBody.scheduled = new Date());
   try {
     await Prisma.entry.update({
       data: req.body,
--- backend/test/server.test.ts
+++ backend/test/server.test.ts
@@ -1,9 +1,9 @@
-import { server } from "../src/server"
-import Prisma from "../src/db";
 import { Entry } from "@prisma/client";
+import Prisma from "../src/db";
+import { server } from "../src/server";
 
 describe("test valid create", () => {
-  it("should create new entry without any errors",async () => {
+  it("should create new entry without any errors", async () => {
     const responce = await server.inject({
       method: "POST",
       url: "/create/",
@@ -15,15 +15,15 @@ describe("test valid create", () => {
       },
     });
     expect(responce.statusCode).toEqual(200);
-    expect(responce.json().title).toEqual("Test")
-    expect(responce.json().description).toEqual("Test")
-    expect(responce.json().created_at).toEqual("2025-02-03T00:00:00.000Z")
-    expect(responce.json().scheduled).toEqual("2025-02-04T00:00:00.000Z")
+    expect(responce.json().title).toEqual("Test");
+    expect(responce.json().description).toEqual("Test");
+    expect(responce.json().created_at).toEqual("2025-02-03T00:00:00.000Z");
+    expect(responce.json().scheduled).toEqual("2025-02-04T00:00:00.000Z");
   });
 });
 
 describe("test invalid create", () => {
-  it("should create an invalid entry and return a 500 error",async () => {
+  it("should create an invalid entry and return a 500 error", async () => {
     const responce = await server.inject({
       method: "POST",
       url: "/create/",
@@ -39,7 +39,7 @@ describe("test invalid create", () => {
 });
 
 describe("test valid get", () => {
-  it("should create new entry and return it with the get method",async () => {
+  it("should create new entry and return it with the get method", async () => {
     // Insert new entry
     const responce = await server.inject({
       method: "POST",
@@ -52,24 +52,24 @@ describe("test valid get", () => {
       },
     });
     // Get entry id
-    const id = responce.json().id
-    console.log(id)
-    
+    const id = responce.json().id;
+    console.log(id);
+
     // Carry out get method and assert equal
     const get_responce = await server.inject({
       method: "GET",
       url: `/get/${id}`,
     });
     expect(get_responce.statusCode).toEqual(200);
-    expect(get_responce.json().title).toEqual("Test2")
-    expect(get_responce.json().description).toEqual("Test2")
-    expect(get_responce.json().created_at).toEqual("2025-02-03T00:00:00.000Z")
-    expect(get_responce.json().scheduled).toEqual("2025-02-04T00:00:00.000Z")
+    expect(get_responce.json().title).toEqual("Test2");
+    expect(get_responce.json().description).toEqual("Test2");
+    expect(get_responce.json().created_at).toEqual("2025-02-03T00:00:00.000Z");
+    expect(get_responce.json().scheduled).toEqual("2025-02-04T00:00:00.000Z");
   });
 });
 
 describe("test valid update", () => {
-  it("should create new entry, update it and get the updated entry",async () => {
+  it("should create new entry, update it and get the updated entry", async () => {
     // Insert new entry
     const responce = await server.inject({
       method: "POST",
@@ -82,8 +82,8 @@ describe("test valid update", () => {
       },
     });
     // Get entry id
-    const id = responce.json().id
-    console.log(id)
+    const id = responce.json().id;
+    console.log(id);
 
     // Update entry
     const update_responce = await server.inject({
@@ -97,22 +97,22 @@ describe("test valid update", () => {
       },
     });
     expect(update_responce.statusCode).toEqual(200);
-    
+
     // Carry out get method and assert equal
     const get_responce = await server.inject({
       method: "GET",
       url: `/get/${id}`,
     });
     expect(get_responce.statusCode).toEqual(200);
-    expect(get_responce.json().title).toEqual("Test3update")
-    expect(get_responce.json().description).toEqual("Test3update")
-    expect(get_responce.json().created_at).toEqual("2025-02-03T00:00:00.000Z")
-    expect(get_responce.json().scheduled).toEqual("2025-02-06T00:00:00.000Z")
+    expect(get_responce.json().title).toEqual("Test3update");
+    expect(get_responce.json().description).toEqual("Test3update");
+    expect(get_responce.json().created_at).toEqual("2025-02-03T00:00:00.000Z");
+    expect(get_responce.json().scheduled).toEqual("2025-02-06T00:00:00.000Z");
   });
 });
 
 describe("test invalid get", () => {
-  it("should try to get non-existant entry and return a 500 error",async () => {
+  it("should try to get non-existant entry and return a 500 error", async () => {
     const get_responce = await server.inject({
       method: "GET",
       url: `/get/99999999`,
@@ -122,8 +122,8 @@ describe("test invalid get", () => {
 });
 
 describe("test invalid update", () => {
-  it("should try to update non-existant id and return a 500 error",async () => {
-      const update_responce = await server.inject({
+  it("should try to update non-existant id and return a 500 error", async () => {
+    const update_responce = await server.inject({
       method: "PUT",
       url: `/update/888888`,
       payload: {
